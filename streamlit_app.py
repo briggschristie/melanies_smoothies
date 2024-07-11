27a2d814a154214af0eec198f0b7c98665161590
briggschristie <briggs.christie@hawaiianair.com>
2024-07-10 14:26:03 -1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -50,5 +50,5 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 	    
         st.success('Your Smoothie is ordered', icon="✅")
-import requests
+
 
